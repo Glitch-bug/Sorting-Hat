e2809eb946cd036904824259c0f3f9beb7f7b6df
six yrn <kwabenayiadom271@gmail.com>
2021-05-11 23:48:08 +0000

    First Tables 2

--- server.py
+++ server.py
@@ -1,15 +1,50 @@
 #_*_ coding: utf-8 _*_
 import os
-from sorting_bot import telegram_chatbot
+import re
 
-path = os.path.abspath('.\config.cfg')
-bot = telegram_chatbot(path)
+from sorting_bot import telegram_chatbot
+from dbmanager import datamanager
+
+#File paths
+config_path = os.path.abspath('.\config.cfg')
+db_path = os.path.abspath('.\database.sql')
+
+#Class calls
+bot = telegram_chatbot(config_path)
+db = datamanager(db_path)
+
+#Variable definitions
 update_id = None
+reply = None 
+
+#Commands and Reponses tuples
+messages = ('/synthesize tables', '/Houses')
+responses = (('...', '...', 'Tables systhesized', 'Tables already in existense'),)
+
+#Tuples of Houses
+houses = ('Gryffindor', 'Hufflepuff', 'Slytherin', 'Ravenclaw')
 
 def make_reply(msg):
     """Provides a reply when messages are recieved"""
-    if msg:
-        reply = "Functional"
+    reply = None 
+
+    if msg is not None:
+        for i in range(len(messages)):
+            if msg == messages[i] and i == 0:
+                tables = db.create_tables(houses)
+                print(tables)
+                if tables == True:
+                    for j in range(len(responses[i]) - 1):
+                        #can use join and split functions to create softer code?? at least in future instances
+                        bot.send_message(responses[i][j], from_)
+                else:
+                    reply = responses[i][(len(responses[i])-1)]
+                break
+            elif msg == messages[i] and i == 1:
+                house_info = db.house_info()
+                reply = 'House ID | HOUSE  | SCORE \n'
+                for house in house_info:
+                    reply += f'{house[0]} | {house[1]} | {house[2]}\n'
     return reply
 
 #Checks for updates to messages and passes re to make reply function
@@ -39,4 +74,4 @@ while True:
         except KeyError:
             message = None
             from_ = None
-        bot.send_message(reply, from_)
+        bot.send_message(reply, from_) 
